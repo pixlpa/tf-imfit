cf297cfa2fd7566a9ee6e8a9039ec537f82ebf69
Andrew Benson <andrewb@cycling74.com>
2020-11-09 01:14:36 -0800

    Update dofit.sh

--- dofit.sh
+++ dofit.sh
@@ -9,10 +9,16 @@ time python tf-imfit/rgb-imfit.py $SOURCEIMG -w source_weights/$NAME-wt.png \
      -o results/weights_lores.txt \
      -p 512 -S -x results/lores/out -n 128
 
+rm -f results/lores/*.png && \
+time python tf-imfit/rgb-imfit.py $SOURCEIMG -w source_weights/$NAME-wt.png \
+     -s 96 -T 256 -a 0.04 \
+     -i results/weights_lores.txt -o results/weights_midres.txt \
+     -p 512 -S -x results/midres/out
+
 rm -f results/hires/*.png && \
 time python tf-imfit/rgb-imfit.py $SOURCEIMG -w source_weights/$NAME-wt.png \
      -s 128 -T 128 -a 0.02 -R 0.0002 \
-     -i results/weights_lores.txt -o results/weights_hires.txt \
+     -i results/weights_midres.txt -o results/weights_hires.txt \
      -p 512 -S -x results/hires/out -n 128
 
 rm -f results/final/*.png && \
