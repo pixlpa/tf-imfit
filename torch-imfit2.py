fix frequency scaling

torch-imfit2.py
@@ -81,7 +81,7 @@ class GaborLayer(nn.Module):
         ))
         
         # Safe sinusoid computation with frequency scaling
-        freq = torch.exp(self.rel_freq) / base_size # Positive frequency scaling
+        freq = torch.exp(self.rel_freq) * base_size # Positive frequency scaling
         phase = self.psi*2*np.pi
         sinusoid = torch.cos(2 * np.pi * freq[:,None,None,None] * x_rot[:, None, :, :] + 
                            phase[:, :, None, None])