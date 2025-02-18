b538cc6f977965d9eae0a65ca7a10b8d7174a08a
Andrew Benson <pixlpa@gmail.com>
2025-02-18 10:46:54 -0800

    tweaking constraint loss

--- torch/torch-imfit3.py
+++ torch/torch-imfit3.py
@@ -401,7 +401,7 @@ class ImageFitter:
 
         # Sum across the mini-batch (n)
         con_loss_per_fit = torch.mean(con_losses, dim=1)
-        con_loss = con_loss_per_fit.mean() / 10  # Use PyTorch's mean
+        con_loss = con_loss_per_fit.mean() / 50  # Use PyTorch's mean
         return con_loss
 
     def train_step(self, iteration, max_iterations):
