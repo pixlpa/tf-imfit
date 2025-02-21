ba531c91f71436c35250e603065834a053161af3
Andrew Benson <pixlpa@gmail.com>
2025-02-21 09:56:56 -0800

    tweaks to the script

--- dofit_torch.sh
+++ dofit_torch.sh
@@ -5,14 +5,14 @@ echo $SOURCEIMG $NAME $NAME.txt
 
 rm -f results/lores/*.png && \
 time python tf-imfit/torch/torch-imfit5.py $SOURCEIMG \
-    --iterations 2000 --single-iterations 20 --output-dir results/lores/ \
-    --size 128 --num-gabors 256 --phase-split 0.6 --global-lr 0.02 \
-    --local-lr 0.005 --mutation-strength 0.0002
+    --iterations 1000 --single-iterations 20 --output-dir results/lores/ \
+    --size 128 --num-gabors 256 --phase-split 0.5 --global-lr 0.02 \
+    --local-lr 0.005 --mutation-strength 0.0005
 
 rm -f results/final/*.png && \
 time python tf-imfit/torch/torch-imfit4.py $SOURCEIMG \
-    --iterations 4000 --single-iterations 10 --output-dir results/final/ \
-    --size 256 --num-gabors 256 --phase-split 0.6 --global-lr 0.02 \
+    --iterations 2000 --single-iterations 10 --output-dir results/final/ \
+    --size 256 --num-gabors 256 --phase-split 0.4 --global-lr 0.01 \
     --local-lr 0.005 --mutation-strength 0.0002 --init results/lores/saved_model.pth \
     --init-size 128
 
