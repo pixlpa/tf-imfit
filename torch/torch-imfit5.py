c35834c364e3ee43eec8c408ed033012158e9b8a
Andrew Benson <pixlpa@gmail.com>
2025-02-21 00:28:36 -0800

    adding more rounds

--- torch/torch-imfit5.py
+++ torch/torch-imfit5.py
@@ -610,7 +610,15 @@ def main():
                 temp = fitter.current_temp
                 pbar.set_postfix(loss=f"{loss:.6f}", temp=f"{temp:.3f}")
                 pbar.update(10)
-        for b in range(100):
+        for b in range(10):
+            fitter.single_optimize(np.random.randint(0, args.num_gabors-1),args.single_iterations)
+        for a in range(200):
+            loss = fitter.train_step(a, 200)
+            if a % 10 == 0:
+                temp = fitter.current_temp
+                pbar.set_postfix(loss=f"{loss:.6f}", temp=f"{temp:.3f}")
+                pbar.update(10)
+        for b in range(10):
             fitter.single_optimize(np.random.randint(0, args.num_gabors-1),args.single_iterations)
         for i in range(args.iterations):
             loss = fitter.train_step(i, args.iterations)    
