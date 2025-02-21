b3f10cd2980b86926dabfd1dd144ac088b3cadd2
Andrew Benson <pixlpa@gmail.com>
2025-02-21 09:47:33 -0800

    use #4 for hi-res output

--- dofit_torch.sh
+++ dofit_torch.sh
@@ -5,13 +5,13 @@ echo $SOURCEIMG $NAME $NAME.txt
 
 rm -f results/lores/*.png && \
 time python tf-imfit/torch/torch-imfit5.py $SOURCEIMG \
-    --iterations 1000 --single-iterations 5 --output-dir results/lores/ \
+    --iterations 2000 --single-iterations 20 --output-dir results/lores/ \
     --size 128 --num-gabors 256 --phase-split 0.6 --global-lr 0.02 \
     --local-lr 0.005 --mutation-strength 0.0002
 
 rm -f results/final/*.png && \
-time python tf-imfit/torch/torch-imfit5.py $SOURCEIMG \
-    --iterations 2000 --single-iterations 5 --output-dir results/final/ \
+time python tf-imfit/torch/torch-imfit4.py $SOURCEIMG \
+    --iterations 4000 --single-iterations 10 --output-dir results/final/ \
     --size 256 --num-gabors 256 --phase-split 0.6 --global-lr 0.02 \
     --local-lr 0.005 --mutation-strength 0.0002 --init results/lores/saved_model.pth \
     --init-size 128
