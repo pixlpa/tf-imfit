44326d4488c645d59c8ca42407da85c4a8432146
Andrew Benson <pixlpa@gmail.com>
2025-02-21 15:58:57 -0800

    redo script for the new version

--- dofit_torch.sh
+++ dofit_torch.sh
@@ -3,18 +3,10 @@ NAME=$(basename $SOURCEIMG .png)
 
 echo $SOURCEIMG $NAME $NAME.txt
 
-rm -f results/lores/*.png && \
-time python tf-imfit/torch/torch-imfit5.py $SOURCEIMG \
-    --iterations 1000 --single-iterations 20 --output-dir results/lores/ \
-    --size 128 --num-gabors 256 --phase-split 0.5 --global-lr 0.02 \
-    --local-lr 0.005 --mutation-strength 0.0005
-
-rm -f results/final/*.png && \
-time python tf-imfit/torch/torch-imfit4.py $SOURCEIMG \
-    --iterations 2000 --single-iterations 10 --output-dir results/final/ \
-    --size 256 --num-gabors 256 --phase-split 0.4 --global-lr 0.01 \
-    --local-lr 0.005 --mutation-strength 0.0001 --init results/lores/saved_model.pth \
-    --init-size 128
+rm -f results/final/*.png
+time python tf-imfit/torch/torch-imfit3a.py $SOURCEIMG --iterations 3000 \
+    --single-iterations 5 --output-dir results/final/ --size 256 --num-gabors 256 \
+    --global-lr 0.009 --mutation-strength 0.0
 
 mv results/final/saved_weights.txt results/$NAME.txt
 cp results/$NAME.txt /content/drive/My\ Drive/HM/results/
