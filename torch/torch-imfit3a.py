c64b7e9bf41383b9fcf35fe610821be69196f2f4
Andrew Benson <pixlpa@gmail.com>
2025-02-22 23:20:11 -0800

    debug post

--- torch/torch-imfit3a.py
+++ torch/torch-imfit3a.py
@@ -340,6 +340,7 @@ class ImageFitter:
     def sobel_filter(self, image):
         # Ensure image is in the right format (B, C, H, W)
         image = image.unsqueeze(0)
+        print("image shape:",image.shape)
         # Define 3D Sobel kernels
         sobel_x = torch.tensor([
             [[[-1, 0, 1],
