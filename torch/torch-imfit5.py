85ff5014c9e0593c8b86f92f9696d34f71272905
Andrew Benson <pixlpa@gmail.com>
2025-02-23 10:28:16 -0800

    reweight

--- torch/torch-imfit5.py
+++ torch/torch-imfit5.py
@@ -465,11 +465,11 @@ class ImageFitter:
         return con_loss
     
     def loss_function(self, output, target):
-        weighted = self.weighted_loss(output, target, self.weights)*0.2
+        weighted = self.weighted_loss(output, target, self.weights)*0.7
         # unweighted = self.unweighted_loss(output, target)*0.5
         # laplace = self.lap_loss(output,target) * 0.1
-        gradient = self.gradient_loss(output,target) * 0.4
-        sobel = self.sobel_loss(output,target) * 0.4
+        gradient = self.gradient_loss(output,target) * 0.2
+        sobel = self.sobel_loss(output,target) * 0.1
         loss =  weighted + sobel + gradient
         return loss
 
