33ca0af3727cae52cb1ba778be5c6b1f66a55c3d
Andrew Benson <pixlpa@gmail.com>
2025-02-22 23:38:51 -0800

    just mse and sobel loss

--- torch/torch-imfit3a.py
+++ torch/torch-imfit3a.py
@@ -464,12 +464,12 @@ class ImageFitter:
         return con_loss
     
     def loss_function(self, output, target):
-        weighted = self.weighted_loss(output, target, self.weights)*0.6
-        unweighted = self.unweighted_loss(output, target)*0.5
-        laplace = self.lap_loss(output,target) * 0.1
+        weighted = self.weighted_loss(output, target, self.weights)*0.8
+        # unweighted = self.unweighted_loss(output, target)*0.5
+        # laplace = self.lap_loss(output,target) * 0.1
         # gradient = self.gradient_loss(output,target) * 0.3
-        sobel = self.sobel_loss(output,target) * 0.3
-        loss =  weighted + laplace + sobel
+        sobel = self.sobel_loss(output,target) * 0.2
+        loss =  weighted + sobel
         return loss
 
     def train_step(self, iteration, max_iterations):
