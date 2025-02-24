a9fd59e1ad5aed4102b01f229e795a5361dff026
Andrew Benson <pixlpa@gmail.com>
2025-02-23 17:43:00 -0800

    redoing dofit script

--- dofit_torch.sh
+++ dofit_torch.sh
@@ -4,9 +4,9 @@ NAME=$(basename $SOURCEIMG .png)
 echo $SOURCEIMG $NAME $NAME.txt
 
 rm -f results/final/*.png
-time python tf-imfit/torch/torch-imfit3a.py $SOURCEIMG --iterations 3000 \
-    --single-iterations 5 --output-dir results/final/ --size 256 --num-gabors 256 \
-    --global-lr 0.009 --mutation-strength 0.0
+time python tf-imfit/torch/torch-imfit5.py $SOURCEIMG --weight source_weights/$NAME-wt.png --iterations 5000 \
+    --output-dir results/final/ --size 256 --num-gabors 256 \
+    --global-lr 0.006 --mutation-strength 0.0002 --gamma 0.99
 
 mv results/final/saved_weights.txt results/$NAME.txt
 cp results/$NAME.txt /content/drive/My\ Drive/HM/results/
